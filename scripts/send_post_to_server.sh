c25d5fab65bc845e520c8cd830d3af75d5754387
Lucas <lucas@prevision.com.br>
2022-05-29 16:07:49 -0300

    Add

--- scripts/send_post_to_server.sh
+++ scripts/send_post_to_server.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/env sh
-curl --request POST http://www.localhost:8001/db --data-raw '{"batch":[{"operation":"INSERT","name":"luquas","city":"floripa"}]}'
-curl --request POST http://www.localhost:8001/line --data-raw '{"number":2}'
+ADDR=$1
+PORT=$2
+REQUESTS_N=$3
+
+rm -rf /tmp/logs
+
+for i in $(seq $(expr $REQUESTS_N))
+do
+    printf "\n"
+    echo "writing on hermes"
+    echo "{\"batch\":[{\"operation\":\"INSERT\",\"name\":\"name-$i\",\"city\":\"city-$i\"}]}"
+    printf "\n"
+    curl --request POST http://$ADDR:$PORT/db --data-raw "{\"batch\":[{\"operation\":\"INSERT\",\"name\":\"name-$i\",\"city\":\"city-$i\"}]}"
+    printf "\n"
+    echo "reading on hermes"
+    printf "\n"
+    curl --request POST http://$ADDR:$PORT/line --data-raw "{\"number\":-1}"
+    printf "\n"
+done
+
+for i in $(seq $(expr $REQUESTS_N + 2))
+do
+    printf "\n"
+    echo "reading on hermes"
+    printf "\n"
+    curl --request POST http://$ADDR:$PORT/line --data-raw "{\"number\":$i}"
+    printf "\n"
+done
+
+wc -l /tmp/logs/operations.log
+
+cat /tmp/logs/operations.log
